d4fd62e57a7f1032f55a9634bc1ef3be7a3e2887
YoussefJemmane <yous.jemm@gmail.com>
2023-12-19 13:22:50 +0100

    add tarif to nouveau ne page

--- src/app/service/nouveaune/page.tsx
+++ src/app/service/nouveaune/page.tsx
@@ -3,46 +3,13 @@
 import Navbar from "@/components/Navbar";
 
 import React from "react";
-import Image from "next/image";
-import img1 from "../../../../public/IMG20231005165037.jpg";
-import img2 from "../../../../public/imp2.jpg";
-import img3 from "../../../../public/im1.jpg";
 
-
-import { Splide, SplideSlide } from "splide-nextjs/react-splide";
-import "splide-nextjs/splide/dist/css/themes/splide-default.min.css";
-
-import { useState, useRef, useEffect } from "react";
 import Header from "@/components/service/nouveaune/Header";
-
-interface SplideRef {
-  splide: {
-    on: (event: string, callback: () => void) => void;
-    go: (index: number) => void;
-    index: number;
-  };
-}
+import Slider from "@/components/service/nouveaune/Slider";
+import Tarifs from "@/components/service/nouveaune/Tarifs";
 
 const NouveauNe = () => {
-  const images = [img1, img2, img3];
-  const [activeSlide, setActiveSlide] = useState(0);
-  const splideRef = useRef<SplideRef | null>(null);
-
-  const handleImageClick = (index: any) => {
-    if (splideRef.current) {
-      splideRef.current.splide.go(index);
-    }
-  };
-
-  useEffect(() => {
-    if (splideRef.current) {
-      splideRef.current.splide.on('moved', () => {
-        if (splideRef.current) {
-          setActiveSlide(splideRef.current.splide.index);
-        }
-      });
-    }
-  }, []);
+  
 
   return (
     <div>
@@ -51,40 +18,10 @@ const NouveauNe = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-7" id="nouveaune">
         <Header />
         <div className="pt-4">
-          <div className="relative">
-            <div
-              className="h-[745px] bg-cover bg-center"
-            >
-              <Splide ref={splideRef} aria-label="My Favorite Images">
-                {images.map((img, index) => (
-                  <SplideSlide key={index}>
-                    <Image
-                      src={img}
-                      alt={`Image ${index + 1}`}
-                      className="h-[745px] object-scale-down "
-                      loading="lazy"
-                    />
-                  </SplideSlide>
-                ))}
-              </Splide>
-            </div>
-            <div className=" mt-4">
-              <div className="flex justify-around gap-4">
-                {images.map((img, index) => (
-                  <div key={index} onClick={() => handleImageClick(index)} className="">
-                    <Image
-                      src={img}
-                      alt={`Image ${index + 1}`} 
-                      layout="fixed"
-                      className={`object-cover w-[100px] ${index === activeSlide ? '' : 'grayscale'}`}
-                      loading="lazy"
-                    />
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
-
+          <Slider />
+        </div>
+        <div className="pt-4">
+          <Tarifs />
         </div>
       </div>
     </div>
---
+++ src/components/service/nouveaune/Tarifs.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Tarifs = () => {
+    return (
+        <div>
+            <div className="card">
+                <h1 className="services text-3xl text-center w-full">
+                    Séance photo Nouveau né 1200 dh
+                </h1>
+                <p className="text-center">Environ 2h de séance photo</p>
+                <ul className="list-disc list-inside">
+                    <li>3 thèmes variés</li>
+                    <li>Accessoires et tenues sont fournis</li>
+                    <li>15 images retouchées (avec tirage)</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Tarifs
