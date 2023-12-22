a3f55aa7e89b57544674651d4feb2065d58f5d6d
YoussefJemmane <yous.jemm@gmail.com>
2023-12-22 18:47:42 +0100

    fix a typo error of famille page

--- src/components/service/famille/Tarifs.tsx
+++ src/components/service/famille/Tarifs.tsx
@@ -5,14 +5,14 @@ const Tarifs = () => {
         <div>
             <div className="card">
                 <h1 className="services text-3xl text-center w-full">
-                    Séance photo Nouveau né
+                    Séance photo Famille
                 </h1>
                 <h1 className="text-center text-3xl">500 dh</h1>
                 <ul className="list-disc list-inside">
                     <li>Environ 1h de séance photo</li>
-                    <li>3 thèmes variés</li>
-                    <li>Accessoires et tenues sont fournis</li>
-                    <li>15 images retouchées (avec tirage)</li>
+                    <li>15 images traitées </li>
+                    <li>dispose de plusieurs fonds </li>
+                    
                 </ul>
             </div>
         </div>
