9c56cba38659aac8c4d1ee8c0b750db5968d8031
YoussefJemmane <yous.jemm@gmail.com>
2023-12-24 23:39:24 +0100

    fix the SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,15 @@
 import type { Metadata } from 'next'
-
+import Head from 'next/head'
 import './globals.css'
-
+import icon from '../../public/logo-noir.webp'
+import React from 'react'
 
 
 export const metadata: Metadata = {
-  title: 'Afnane Photography',
-  description: 'Découvrez des séances photos uniques pour bébés, familles, grossesses et professionnels. Contactez-nous pour réserver votre expérience inoubliable.',
-  keywords: ' Afnane Photography, Afnane photography,afnane photography, afnanephotography, afnane.photography, @afnanephotography, @afnane.photography, afnane_photography, @afnane_photography, photographe professionnel, séance photo bébé, séance photo famille, séance photo grossesse, séance photo professionnel, Maroc,',
-  creator: 'Youssef Jemmane',
+  title: "Afnane Photography",
+  description: "Découvrez des séances photos uniques pour bébés, familles, grossesses et professionnels. Contactez-nous pour réserver votre expérience inoubliable.",
+  keywords: " Afnane Photography, Afnane photography,afnane photography, afnanephotography, afnane.photography, @afnanephotography, @afnane.photography, afnane_photography, @afnane_photography, photographe professionnel, séance photo bébé, séance photo famille, séance photo grossesse, séance photo professionnel, Maroc,",
+  creator: "Youssef Jemmane",
 
 
 
@@ -20,8 +21,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head >
+        <title>{metadata.title as React.ReactNode}</title>
+        <meta name="description" content={metadata.description as string} />
+        <meta name="keywords" content={metadata.keywords as string} />
+        <meta name="author" content={metadata.creator as string} />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href={icon.src} />
+      </Head>
       <body >{children}</body>
-      
+
     </html>
   )
 }
